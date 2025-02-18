045d7dafa406ee770d480891bde544680a9e46e9
ilyabekirov <ilja.bekirov@gmail.com>
2025-02-19 02:31:20 +0300

    add docker

--- app/services/telegram_bot.rb
+++ app/services/telegram_bot.rb
@@ -9,6 +9,8 @@ class TelegramBot
     puts "secret_key_base: #{Rails.application.secrets.secret_key_base}"
     puts "secret_key_base: #{ENV['SECRET_KEY_BASE']}"
     @bot = Telegram::Bot::Client.new(ENV['TELEGRAM_BOT_TOKEN'])
+    puts 'Бот инициализирован'
+    puts @bot.inspect
   end
 
   def start
