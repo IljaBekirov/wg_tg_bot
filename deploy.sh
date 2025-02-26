1f4e65b799f8a9814fc5c65787cac573a61d66ef
ilyabekirov <ilja.bekirov@gmail.com>
2025-02-26 19:54:37 +0300

    check

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,14 @@ set -e
 echo "Pulling latest changes from GitHub..."
 git pull origin master
 
-echo "Building new Docker image..."
+echo "Stopping running containers..."
+docker-compose down
+
+echo "Building new Docker image with no cache..."
 docker-compose build --no-cache bot
 
-echo "Restarting services..."
-docker-compose up -d --force-recreate bot
+echo "Starting services with forced recreation..."
+docker-compose up -d --force-recreate
 
 echo "Cleaning up unused images..."
 docker image prune -f
